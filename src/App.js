b7b07760aee6bfa919f9722bcf82d24edd413761
Arseniy Pomazkov <arseniy.pomazkov@gmail.com>
2021-11-15 09:02:17 +0300

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import { Component } from 'react'
-import { Navbar, NavbarBrand } from 'reactstrap'
 
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes'
+import Main from './components/MainComponent';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={DISHES} />
+      <div className="App">
+        <Main />
       </div>
     )
   }
--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -23,12 +23,10 @@ export default class DishDetail extends Component {
           <ul className="list-unstyled">
             {
               comments.map((comment) => {
-                const date = new Date(comment.date)
-                const dateArray = date.toDateString().split(" ");
                 return (
                   <li key={comment.id}>
                     <p>{comment.comment}</p>
-                    <p>–– {comment.author}, {dateArray[1]} {dateArray[2]}, {dateArray[3]}</p>
+                    <p>–– {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                   </li>
                 )
               })
@@ -45,12 +43,14 @@ export default class DishDetail extends Component {
   render() {
     if (this.props.dish != null) {
       return (
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.props.dish)}
-          </div>
-          <div className="col-12 col-md-5 m-1">
-          {this.renderComments(this.props.dish.comments)}
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-5 m-1">
+              {this.renderDish(this.props.dish)}
+            </div>
+            <div className="col-12 col-md-5 m-1">
+            {this.renderComments(this.props.dish.comments)}
+            </div>
           </div>
         </div>
       )
