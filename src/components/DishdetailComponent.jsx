dae24b58c838495ef508219d3346d37c77a8fd78
Arseniy Pomazkov <arpomazkov@bk.ru>
2021-12-08 19:26:17 +0300

    Fetch from Server

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -7,6 +7,8 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 
 import { Loading } from './LoadingComponent';
 
+import { baseUrl } from '../shared/baseUrl';
+
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -104,7 +106,7 @@ class CommentForm extends Component {
 function RenderDish({dish}) {
   return(
     <Card>
-      <CardImg top src={dish.image} alt={dish.name} />
+      <CardImg top src={baseUrl + dish.image} alt={dish.name} />
       <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
